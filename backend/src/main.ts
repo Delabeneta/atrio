c474c527a6868b3e579215482cf63c1e3e70b1f4
Delabeneta <crazayfrog@gmail.com>
2026-07-02 18:21:40 -0300

    fix: cors ativado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
       'http://127.0.0.1:5500',
       'http://localhost:5500',
       'http://localhost:3000',
-      'https://atrio-frontend.vercel.app',
+      'https://atrio-nu.vercel.app',
     ],
     // ['http://localhost:3001'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
